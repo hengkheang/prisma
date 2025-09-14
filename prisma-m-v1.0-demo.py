ba522bcdf5843c7d2e014422f818097ecb350508
Hengkheang Thai <hengkheangt@gmail.com>
2025-09-14 14:02:53 +0000

    Add them side by side

--- prisma-m-v1.0-demo.py
+++ prisma-m-v1.0-demo.py
@@ -48,8 +48,7 @@ if st.button("Next") and st.session_state.ready_solution == []:
         st.session_state.unified_text+=f"{k}\n"
 
 if st.session_state.unified_text != [] and st.session_state.input == []:
-    st.session_state.edited_text = st.text_area("Preview", value=st.session_state.unified_text, height=300)
-    st.write(st.session_state.edited_text)
+    col1, col2 = st.session_state.edited_text = st.text_area("Preview", value=st.session_state.unified_text, height=300), st.write(st.session_state.edited_text)
     if st.button("Confirm"):
         st.session_state.input = st.session_state.edited_text
         
