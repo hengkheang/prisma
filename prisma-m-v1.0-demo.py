3e538bb7d524a7aa00d681124039cf917af50e26
Hengkheang Thai <hengkheangt@gmail.com>
2025-09-14 06:32:36 +0000

    Simplify

--- prisma-m-v1.0-demo.py
+++ prisma-m-v1.0-demo.py
@@ -39,39 +39,37 @@ if st.session_state.solution == []:
         st.session_state.solution = uploaded
         st.button("Next")
 else:
-    if st.button("Next"):
-        st.session_state.ready_solution = []
-        for p in st.session_state.solution:
-            p2t = Pix2Text.from_config()
-            outs2 = p2t.recognize(p, file_type='text_formula', return_text=True, save_analysis_res='mixed-out.jpg')
-        for k in st.session_state.ready_solution:
-            st.session_state.unified_text+=f"{k}\n"
-        if st.button("Confirm and Send"):
-            client = InferenceClient(
-                provider="featherless-ai",
-                api_key=os.environ["APITOKEN"],
-            )
+    st.session_state.ready_solution = []
+    for p in st.session_state.solution:
+        p2t = Pix2Text.from_config()
+        outs2 = p2t.recognize(p, file_type='text_formula', return_text=True, save_analysis_res='mixed-out.jpg')
+    for k in st.session_state.ready_solution:
+        st.session_state.unified_text+=f"{k}\n"
+        client = InferenceClient(
+            provider="featherless-ai",
+            api_key=os.environ["APITOKEN"],
+        )
 
-            stream = client.chat.completions.create(
-                model="Qwen/Qwen2.5-Math-1.5B-Instruct",
-                messages=[
-                    {"role": "system","content": "Solve the problem concisely"},
-                    {"role": "system","content": st.session_state.edited_text}
-                ],
-                temperature=0.1,
-                max_tokens=1000,
-                stream=True,
-            )
+        stream = client.chat.completions.create(
+            model="Qwen/Qwen2.5-Math-1.5B-Instruct",
+            messages=[
+                {"role": "system","content": "Solve the problem concisely"},
+                {"role": "system","content": st.session_state.edited_text}
+            ],
+            temperature=0.1,
+            max_tokens=1000,
+            stream=True,
+        )
 
-            for chunk in stream:
-                delta = chunk.choices[0].delta.get("content")
-                if delta:
-                    left, right = st.columns([1, 10])
-                    left.write("🤗")
-                    ph = right.empty()
-                    text = ""
-                    d = chunk.choices[0].delta.content
-                    text += d.replace("\n", " ")
-                    ph.write(text)
-                    time.sleep(0.5)
+        for chunk in stream:
+            delta = chunk.choices[0].delta.get("content")
+            if delta:
+                left, right = st.columns([1, 10])
+                left.write("🤗")
+                ph = right.empty()
+                text = ""
+                d = chunk.choices[0].delta.content
+                text += d.replace("\n", " ")
+                ph.write(text)
+                time.sleep(0.5)
 
