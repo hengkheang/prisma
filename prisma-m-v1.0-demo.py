edef4332ec5f3ecedcea9b78340c24d5b8ca2e49
Hengkheang Thai <hengkheangt@gmail.com>
2025-09-14 14:54:52 +0000

    Fix display problem

--- prisma-m-v1.0-demo.py
+++ prisma-m-v1.0-demo.py
@@ -7,6 +7,7 @@ from pathlib import Path
 from PIL import Image
 from pix2text import Pix2Text, merge_line_texts
 import streamlit.components.v1 as components
+import re
 
 def stop_stream():
     st.session_state.stop = True
@@ -87,7 +88,9 @@ if st.session_state.unified_text != [] and st.session_state.input == []:
             delta = chunk.choices[0].delta.get("content")  # the actual string
             if delta:  
                 text += delta
-                ph.write(text)
+                output_fixed = re.sub(r'\((.*?)\)', r'$\1$', output)
+                output_fixed = re.sub(r'\[(.*?)\]', r'$$\1$$', output_fixed)
+                ph.write(output_fixed)
         #if st.button("Stop", key="stop_button"):
             #st.experimental_rerun()
 
