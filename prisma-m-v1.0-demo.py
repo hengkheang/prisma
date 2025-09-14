17a2e1525eaf19aab675a77f13f2b9fc81b0af54
Hengkheang Thai <hengkheangt@gmail.com>
2025-09-14 14:46:03 +0000

    Fixed indentation again

--- prisma-m-v1.0-demo.py
+++ prisma-m-v1.0-demo.py
@@ -89,8 +89,8 @@ if st.session_state.unified_text != [] and st.session_state.input == []:
                 text += delta
                 ph.write(text)
         st.button("Stop", key="stop_button", on_click=stop_stream)
-        if st.session_state.stop:
-            break
+            if st.session_state.stop:
+                break
 
                 
         
